8b96646c8b9b9e71b9115b32ff104f4005d403a2
Siddhant Chauhan <sc3722@ic.ac.uk>
2023-07-22 10:49:16 +0530

    Added comments and made an initial render

--- SDLGame/main.c
+++ SDLGame/main.c
@@ -2,12 +2,18 @@
 #include <SDL2/SDL.h>
 
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {                                           // Initialize SDL2
         printf("Error initializing SDL: %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("Game Window",                           // window title
+                                          SDL_WINDOWPOS_UNDEFINED,                 // window x position
+                                          SDL_WINDOWPOS_UNDEFINED,                 // window y position
+                                          800,                                     // width, in pixels
+                                          600,                                     // height, in pixels
+                                          SDL_WINDOW_SHOWN                         // flags
+                                          );
     if (!window) {
         printf("Error creating window: %s\n", SDL_GetError());
         SDL_Quit();
@@ -22,8 +28,24 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+    // clear screen to blue
+    SDL_RenderClear(renderer);
+
+    // set drawing colour to red
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+    SDL_Rect rect = {350, 250, 100, 100};
+    SDL_RenderFillRect(renderer, &rect);
+
+    // done drawing, now presenting
+    SDL_RenderPresent(renderer);
+
+    // wait for a while
     SDL_Delay(3000);
 
+    // close and destroy window
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
