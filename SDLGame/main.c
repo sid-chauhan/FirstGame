6ada707183c1be8d1e427c37653f34d0e1612a2c
Kufupa <wilsonianprogrammer@gmail.com>
2023-07-23 23:51:21 +0100

    removed redundant movement code

--- SDLGame/main.c
+++ SDLGame/main.c
@@ -36,12 +36,6 @@ int processEvents(SDL_Window *window, Hero *hero)
             case SDLK_ESCAPE:
                 done = 1;
                 break;
-            case SDLK_LEFT:
-                hero->x -= 10;
-                break;
-            case SDLK_RIGHT:
-                hero->x += 10;
-                break;
             }
             break;
         }
